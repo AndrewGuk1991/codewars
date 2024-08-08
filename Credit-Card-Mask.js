5addf39771962883d20a55518c669e8916830e9d
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-08 20:18:19 +0300

    commit-credit-card-mask

---
+++ Credit-Card-Mask.js
@@ -0,0 +1,16 @@
+// Description:
+
+// Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+// Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+// Solution:
+
+function maskify(cc) {
+    return cc.substring(0, (cc.length - 4)).split('').map(el => el = '#').join('') + cc.substring(cc.length - 4)
+}
+
+
+
+
+
