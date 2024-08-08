6a24ba1dc48323d5150aed4df01ccdf173051257
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-08 19:38:44 +0300

    commit-shortest-word

---
+++ Shortest-Word.js
@@ -0,0 +1,11 @@
+// Description:
+
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+// Solution:
+
+function findShort(s) {
+    return (s.split(' ').map(el => el.length).sort((a, b) => a - b))[0]
+}
