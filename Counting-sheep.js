aad6f0fc4d7bcf202e2bf0a26658006cddd5c606
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-10 23:15:32 +0300

    commit-countSheeps

---
+++ Counting-sheep.js
@@ -0,0 +1,9 @@
+// Description:
+
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+// Solution:
+
+function countSheeps(sheep) {
+    return sheep.filter(el => el === true).length
+}
