91cd3b3655956c8415595ec465891801f081ec8b
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-10 23:23:00 +0300

    commit-noSpace

---
+++ Remove-String-Spaces.js
@@ -0,0 +1,10 @@
+// Description:
+
+// Write a function that removes the spaces from the string, then return the resultant string.
+
+// Solution:
+
+function noSpace(x){
+    return x.split(' ').join('')
+}
+
