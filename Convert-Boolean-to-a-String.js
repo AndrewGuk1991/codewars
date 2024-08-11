8f88f737113a78f2fb697c15371e5a9bd1f0e1a4
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-11 11:06:43 +0300

    commit-booleanToString

---
+++ Convert-Boolean-to-a-String.js
@@ -0,0 +1,9 @@
+// Description:
+
+// Implement a function which convert the given boolean value into its string representation.
+
+// Solution:
+
+function booleanToString(b){
+    return String(b)
+  }
