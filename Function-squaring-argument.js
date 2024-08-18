39f378cf7ad82a96570270a680751bb172e1ef28
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-18 16:05:10 +0300

    commit-function-squaring-argument

---
+++ Function-squaring-argument.js
@@ -0,0 +1,9 @@
+// Description:
+
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// Solution:
+
+function square (arg) {
+    return Math.pow(arg, 2)
+}
