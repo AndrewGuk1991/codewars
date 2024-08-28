746e5187508185eeb67bba7e81511be19445e2eb
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-28 17:57:55 +0300

    commit-nameShuffler

---
+++ Name-Shuffler.js
@@ -0,0 +1,9 @@
+// Description: 
+
+// Write a function that returns a string in which firstname is swapped with last name.
+
+// Solution: 
+
+function nameShuffler(str) {
+    return str.split(' ').reverse().join(' ')
+}
