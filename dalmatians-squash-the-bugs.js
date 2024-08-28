bedac4d943b98d36d67d38a80194097b93d5ca37
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-28 21:02:45 +0300

    commit-101-Dalmatians-squash-the-bugs-not-the-dogs

---
+++ dalmatians-squash-the-bugs.js
@@ -0,0 +1,13 @@
+// Description: 
+
+function howManyDalmations(number) {
+  
+    const dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIONS!!!"];
+    
+    
+  return number <= 10 ? dogs[0] : number <= 50 ? dogs[1] : number === 101 ? dogs[3] : dogs[2]
+}
+
+console.log(howManyDalmations(101));
+
+
