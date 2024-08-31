a037499d9d4d7bb9be84da7e36c2d2313c3f67d3
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-31 20:55:31 +0300

    commit-unusualFive

---
+++ 5-without-numbers.js
@@ -0,0 +1,14 @@
+// Description: 
+
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+// Solution: 
+
+function unusualFive() {
+    const str = 'five_'
+    return str.length
+}
