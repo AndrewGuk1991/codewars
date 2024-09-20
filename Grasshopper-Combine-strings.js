acd766829e152e2c896a664a104e365f2e4242bd
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-20 19:47:06 +0300

    commit-Grasshopper-Combine-strings

---
+++ Grasshopper-Combine-strings.js
@@ -0,0 +1,9 @@
+// Description: 
+
+// Create a function named combineNames/combine_names/CombineNames that accepts two parameters (first and last name). The function should return the full name.
+
+// Solution: 
+
+function combineNames(first, last_name) {
+    return first + " " + last_name
+}
