f76e6007f958f3d2cd8ce76ce82ad4d22b0ed8f9
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-26 19:59:04 +0300

    commit-Kata-Example-Twist

---
+++ Kata-Example-Twist.js
@@ -0,0 +1,10 @@
+// Description: 
+
+// Add the value "codewars" to the array websites 1,000 times.
+
+// Solution: 
+
+var websites = []
+for(let i = 0; i < 1000; i++) {
+  websites.push('codewars')
+}
