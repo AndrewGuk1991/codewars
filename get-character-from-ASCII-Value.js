79d3c975354c11cf2658fe7c3df64666edfe6920
AndrewGuk1991 <gukandrey91@gmail.com>
2024-10-22 20:03:01 +0300

    commit-getChar

---
+++ get-character-from-ASCII-Value.js
@@ -0,0 +1,9 @@
+// Description: 
+
+// Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+// Solution: 
+
+function getChar(c) {
+    return String.fromCharCode(c)
+}
