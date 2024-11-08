b1fd5b2f48c5ede6005b8a28b2bfd27a862f26aa
AndrewGuk1991 <gukandrey91@gmail.com>
2024-11-08 10:01:10 +0300

    commit-getMinMax

---
+++ Find-min-and-max.js
@@ -0,0 +1,10 @@
+// Description: 
+
+// Implement a function that returns the minimal and the maximal value of a list (in this order).
+
+// Solution: 
+
+function getMinMax(arr) {
+    arr.sort((a, b) => a - b)
+    return [arr[0], arr[arr.length - 1]]
+};
