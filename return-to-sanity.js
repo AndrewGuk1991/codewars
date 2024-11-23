481751c95181a6748e7054e69d40f6ea418c5a2a
AndrewGuk1991 <gukandrey91@gmail.com>
2024-11-23 19:52:49 +0300

    commit-return-to-sanity

---
+++ return-to-sanity.js
@@ -0,0 +1,10 @@
+// Description: 
+
+// This function should return an object, but it's not doing what's intended. What's wrong?
+
+// Solution: 
+
+function mystery() {
+    var results = { sanity: 'Hello' }
+    return results
+}
